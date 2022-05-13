62bf10facf7cc28b756e5264e77a9c2ba4e75509
Matt Eilar <meilar@gmail.com>
2022-05-13 12:31:15 -0700

    Add Home view

---
+++ Octavo/HomeScreenView.swift
@@ -0,0 +1,48 @@
+//
+//  SwiftUIView.swift
+//  Octavo
+//
+//  Created by Matt Eilar on 5/13/22.
+//
+
+import SwiftUI
+
+struct HomeScreenView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.purple, .black, .mint]), startPoint: .topLeading, endPoint: .bottomTrailing)
+            VStack {
+                Text("Octavo")
+                    .font(.system(size:48, weight: .light, design: .serif))
+                    .foregroundColor(.white)
+                HStack {
+                    RoundedRectangle(cornerRadius:10, style: .continuous).stroke(Color.white, lineWidth: 2)
+                        .padding()
+                        .frame(width: 150, height: 100, alignment: .topLeading)
+                        .overlay(
+                            Text("Reading Log")
+                                .foregroundColor(.white)
+                        )
+                }
+                HStack {
+                    RoundedRectangle(cornerRadius:10, style: .continuous).stroke(Color.white, lineWidth: 2)
+                        .padding()
+                        .frame(width: 150, height: 100, alignment: .topLeading)
+                        .overlay(
+                            Text("Book List")
+                                .foregroundColor(.white)
+                        )
+                }
+            }
+            .padding()
+    
+        }
+
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeScreenView()
+    }
+}
